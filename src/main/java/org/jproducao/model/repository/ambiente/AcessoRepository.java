0b9751f5e883875b6e8a7e814b224c608c060d1a
cledsonfs-cmd <cledsonfs@gmail.com>
2022-10-30 10:12:11 -0300

    Alteração de class para interface Repository/ambiente

--- src/main/java/org/jproducao/model/repository/ambiente/AcessoRepository.java
+++ src/main/java/org/jproducao/model/repository/ambiente/AcessoRepository.java
@@ -4,4 +4,5 @@ import org.jproducao.model.entity.ambiente.Acesso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AcessoRepository extends JpaRepository<Acesso,Integer> {
+
 }
